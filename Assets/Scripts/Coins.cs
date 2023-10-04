00149ff35eba88a75ff25d093f85af46cb8b6ced
dylanJasperX1 <djasper5983@student.pps.net>
2023-10-04 12:01:19 -0700

    Coin work
    Coin work

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COINS : MonoBehaviour
+{
+    public int coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.CompareTag("Coin"))
+        {
+            Debug.Log("Coin collected");
+
+        }
+    }
+}
