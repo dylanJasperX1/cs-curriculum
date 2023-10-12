0bda681b1b374d0696b0542aacd15ca5c7fe46ed
dylanJasperX1 <djasper5983@student.pps.net>
2023-10-12 10:06:23 -0700

    Coins
    Functional coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Coin : MonoBehaviour
 {
     public int coins;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        Debug.Log("Initial coins: " + coins);
     }
 
     // Update is called once per frame
@@ -19,10 +22,11 @@ public class Coin : MonoBehaviour
     
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Coin"))
         {
-            Debug.Log("Coin collected");
-            gameObject.SetActive(false);
+            coins += 1;
+            Debug.Log("Coins: " + coins);
+            Destroy(collision.gameObject);
         }
     }
 }
