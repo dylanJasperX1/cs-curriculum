20d1615644550b173a4b1b50e67eee4b9a29e13c
dylanJasperX1 <djasper5983@student.pps.net>
2023-10-17 11:41:18 -0700

    Nonfunctioning spikes
    Nonfunctioning spikes

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+    public int health = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,13 @@ public class Spike : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spikes"))
+        {
+            Debug.Log("Touching spike");
+        }
     }
 }
