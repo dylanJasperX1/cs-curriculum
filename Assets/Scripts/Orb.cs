c479a3423ce205845832600b0e1e92617614f7f5
dylanJasperX1 <djasper5983@student.pps.net>
2023-11-30 11:27:49 -0800

    Demon
    Nonfunctioning demon

---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Orb : MonoBehaviour
+{
+    public Spike health;
+    public HUD hud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
